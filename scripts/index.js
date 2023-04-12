c1b6a07f40620de4245e23f0b22cc4f6fe79f535
ElyA_I <elyaizmaylova@gmail.com>
2023-04-12 12:37:19 +0300

    Feat: 6-card template added by JavaScript using ready-made array

--- scripts/index.js
+++ scripts/index.js
@@ -37,3 +37,89 @@ function handleFormSubmit (evt) {
 editButton.addEventListener('click', openPopup);
 popupCloseBtn.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit); 
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    liked: false
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    liked: false
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    liked: false
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    liked: false
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    liked: false
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    liked: false
+  }
+];
+
+const elementsList = document.querySelector('.elements__list');
+
+function render(elements = [], parent) {
+  clearTemplate(parent);
+
+  const template = document.querySelector('#elements-template')
+
+  elements.forEach(element => {
+    const card = template.content.cloneNode(true);
+
+    createImage(card, element);
+    createCaption(card, element);
+    createLike(card, element);
+
+    parent.appendChild(card);
+  });
+}
+
+function clearTemplate(parent) {
+  while (parent.lastElementChild) {
+    parent.removeChild(parent.lastElementChild)
+  }
+}
+
+function createImage(parent, element) {
+  const img = parent.querySelector('.element__image');
+  img.src = element.link;
+  img.alt = element.name;
+}
+
+function createCaption(parent, element) {
+  const name = parent.querySelector('.element__name');
+  name.textContent = element.name;
+}
+
+function createLike(parent, element) {
+  const like = parent.querySelector('.element__like');
+
+  if (element.liked) {
+    like.classList.add('element__like_active');
+  } else {
+    like.classList.remove('element__like_active');
+  }
+
+  like.addEventListener('click', () => {
+    element.liked = !element.liked;
+    render(initialCards, elementsList);
+  });
+}
+
+render(initialCards, elementsList);
+
