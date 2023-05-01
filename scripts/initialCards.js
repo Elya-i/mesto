7956e2144b75edb3bfe6af5dcd21924881d8448e
ElyA_I <elyaizmaylova@gmail.com>
2023-05-01 21:04:14 +0200

    Export constant initialCards

---
+++ scripts/initialCards.js
@@ -0,0 +1,8 @@
+export const initialCards = [
+  {name: 'Архыз', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg', alt: 'Изображение гор покрытых зеленью и снегом'},
+  {name: 'Челябинская область', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg', alt: 'Изображение реки c заснеженными берегами'},
+  {name: 'Иваново', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg', alt: 'Изображение многоэтажных зданий в вечернее время'},
+  {name: 'Камчатка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg', alt: 'Изображение растительности на склоне вулкана Камчатки'},
+  {name: 'Холмогорский район', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg', alt: 'Изображение железной дороги вдоль деревьев'},
+  {name: 'Байкал', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg', alt: 'Изображение зимнего скалистого берега озера Байкал'}
+]
